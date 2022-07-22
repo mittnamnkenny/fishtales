a4ad08add20167eb68f3afb313aa850ea91d07db
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-07-22 10:23:26 +0000

    Add code to render PostList view and connect home and blog urls

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from . import views
+from django.urls import path
+from django.views.generic import TemplateView
+
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('blog/', views.PostList.as_view(), name='blog'),
+]
