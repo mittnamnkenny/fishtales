ab248b9ae35c0e110ec19fa6af300efca0da9d3b
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-07-22 11:10:03 +0000

    Update post_detail template and connect the url for post detail view

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('blog/', views.PostList.as_view(), name='blog'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
