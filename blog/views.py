d22e3cc9eebb4d1b8f2f2a7946268cd86337a5e8
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-08-11 09:22:33 +0000

    Update views for PostDetail and PostLike with login check after code review

--- blog/views.py
+++ blog/views.py
@@ -66,37 +66,22 @@ class PostDetail(View):
         post detail page
         Success message as user feedback
         """
-        queryset = Post.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by('-created_on')
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
+        if request.user.is_authenticated:
+            queryset = Post.objects.filter(status=1)
+            post = get_object_or_404(queryset, slug=slug)
+            comment_form = CommentForm(data=request.POST)
 
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
-            comment = comment_form.save(commit=False)
-            comment.post = post
-            comment.author = request.user
-            comment.save()
-            messages.success(request, 'Comment Added')
-        else:
-            comment_form = CommentForm()
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.author = request.user
+                comment.save()
+                messages.success(request, 'Comment Added')
 
-        return render(
-            request,
-            'post_detail.html',
-            {
-                'post': post,
-                'comments': comments,
-                'commented': True,
-                'comment_form': CommentForm(),
-                'liked': liked
-            },
-        )
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
 
 
-class PostLike(View):
+class PostLike(LoginRequiredMixin, View):
     """
     View to remove or add like on post detail page
     """
