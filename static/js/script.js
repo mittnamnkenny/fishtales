8f8ef98fed4992bfa585fb5cad095becbe75d4b0
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-08-15 10:04:05 +0000

    Move embedded javascript to separate file in static folder

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+// Remove flash message alert after 2.5 seconds
+document.addEventListener("DOMContentLoaded", removeAlerts);
+
+function removeAlerts() {
+    setTimeout(function () {
+        let messages = document.getElementById('msg');
+        let alert = new bootstrap.Alert(messages);
+        alert.close();
+    }, 2500);
+}
